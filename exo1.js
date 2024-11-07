baddfa7a012abacb0b354c07ac48127d596dbfc5
Mvittorelli <mvittorelli@edenschool.fr>
2024-11-07 09:03:18 +0100

    feat(app):function attentat

---
+++ exo1.js
@@ -0,0 +1,9 @@
+function sumTwoNumbers(a, b){
+    return a + b;
+  }
+  
+  console.log(sumTwoNumbers(3, 7)); // 10
+  console.log(sumTwoNumbers(-1, 5)); // 4
+  console.log(sumTwoNumbers(0, 0)); // 0
+  console.log(sumTwoNumbers(10, -10)); // 0
+  console.log(sumTwoNumbers(1.5, 2.5)); // 4
